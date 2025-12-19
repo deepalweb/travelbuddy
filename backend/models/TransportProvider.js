4d69b2c44887b497dda5def21641ecc61939e483
deepalweb <deepal.web@gmail.com>
2025-12-19 10:48:05 +0530

    Add unique constraint to transport provider email

--- backend/models/TransportProvider.js
+++ backend/models/TransportProvider.js
@@ -5,7 +5,7 @@ const transportProviderSchema = new mongoose.Schema({
   companyLogo: String,
   companyName: { type: String, required: true },
   ownerName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   address: { type: String, required: true },
   description: String,
