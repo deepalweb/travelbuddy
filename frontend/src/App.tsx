0316e1b219c55b9a0a4bc5e7a9a08254fc8b17d5
deepalweb <deepal.web@gmail.com>
2026-02-21 15:17:01 +0530

    Fix: Replace ComprehensiveAdmin with AdminPanel in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ import { checkFirebaseStatus } from './utils/firebaseStatus'
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage'
 import TermsOfServicePage from './pages/TermsOfServicePage'
 import { NotFoundPage } from './pages/NotFoundPage'
-import ComprehensiveAdmin from './pages/ComprehensiveAdmin'
+import { AdminPanel } from './pages/AdminPanel'
 
 const AppContent: React.FC = () => {
   const { config, loading, error } = useConfig()
@@ -82,7 +82,7 @@ const AppContent: React.FC = () => {
             <Route path="/trips/:id" element={<TripDetailPage />} />
             <Route path="/places/:id" element={<PlaceDetailsPage />} />
             <Route path="/community/story/:id" element={<StoryDetailPage />} />
-            <Route path="/admin" element={<ComprehensiveAdmin />} />
+            <Route path="/admin" element={<AdminPanel />} />
 
             <Route path="/*" element={<Layout />} />
             <Route path="*" element={<NotFoundPage />} />
